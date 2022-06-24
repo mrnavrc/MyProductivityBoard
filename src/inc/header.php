436d1357c68d6a176351ede10f602addb6312202
mrnavrc <mrnavratil16@gmail.com>
2022-06-24 22:11:08 +0200

    bootstrap and vue

--- src/inc/header.php
+++ src/inc/header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<button onclick="window.location.href='../../public/todo.php';">
+<button class="btn btn-primary btn-sm" onclick="window.location.href='../../public/todo.php';">
     ToDo
 </button>
-<button onclick="window.location.href='../../public/notepad.php';">
+<button class="btn btn-primary btn-sm" onclick="window.location.href='../../public/notepad.php';">
     Notepad
 </button>
-<button onclick="window.location.href='../../public/logout.php';">
+<button class="btn btn-primary btn-sm" onclick="window.location.href='../../public/logout.php';">
     logout
 </button>
