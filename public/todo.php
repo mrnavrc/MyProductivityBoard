eeaccaa24af8c885ffb68fc3105d7cb639b30312
mrnavrc <mrnavratil16@gmail.com>
2022-06-25 16:46:57 +0200

    bootstrap design todo

--- public/todo.php
+++ public/todo.php
@@ -23,33 +23,51 @@ if (isset($_POST['submit_todo'])) {
     <title>ToDo app</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-            crossorigin="anonymous"></script>
-
 </head>
 <body>
 <?php
 include '../src/inc/header.php'
 ?>
+<style>
+    #mainSection {
+        padding-top: 60px;
+        justify-content: center;
+    }
+</style>
+<section id="mainSection">
+    <div class="container">
+        <h1 class="display-5">To-Do App</h1>
+        <form action="" method="post">
+            <input type="text" name="todo" placeholder="To-Do">
+            <input type="date" name="todo_date" placeholder="To-Do Date">
+            <br>
+            <input type="submit" class="btn btn-primary mt-2" name=" submit_todo" value="Save">
+        </form>
+        <h1 class="display-6">Weekly To-Do</h1>
+        <div class="container border border-dark">
+            <div class="row">
+                <div class="col-2">lorem</div>
+                <div class="col-2">lorem</div>
+                <div class="col-2">lorem</div>
+                <div class="col-2">lorem</div>
+                <div class="col-2">lorem</div>
+                <div class="col-2">lorem</div>
+            </div>
+        </div>
+        <h1 class="display-6">Quick To-Do</h1>
+        <div class="container border border-dark">
+            <div class="row">
+                <div class="col-4">lorem</div>
+                <div class="col-4">lorem</div>
+                <div class="col-4">lorem</div>
+            </div>
+        </div>
+    </div>
+</section>
 
-<h1>ToDo App</h1>
-<form action="" method="post">
-    <input type="text" name="todo" placeholder="To-Do">
-    <input type="date" name="todo_date" placeholder="To-Do Date">
-    <input type="submit" class="btn btn-primary btn-sm" name=" submit_todo" value="Save">
-</form>
-<h2>ToDo List</h2>
 
-<div class="7dCalendar">
-
-</div>
-
-<div class="quickToDo">
-
-
-</div>
-
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
 </body>
 </html>
--- src/inc/header.php
+++ src/inc/header.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-<button class="btn btn-primary btn-sm" onclick="window.location.href='../../public/todo.php';">
-    ToDo
-</button>
-<button class="btn btn-primary btn-sm" onclick="window.location.href='../../public/notepad.php';">
-    Notepad
-</button>
-<button class="btn btn-primary btn-sm" onclick="window.location.href='../../public/logout.php';">
-    logout
-</button>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ToDo app</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+<ul class="nav nav-tabs nav-justified">
+    <li class="nav-item">
+        <a class="nav-link" href="../../public/todo.php">To-Do</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="../../public/notepad.php">Notepad</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-black" href="../../public/logout.php">Logout</a>
+    </li>
+</ul>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
+</body>
+</html>
